de8e23a641bd920c36930a219aabefacda4b7bfa
prashantjinwal <prashantjinwal888@gmail.com>
2025-12-28 14:32:40 +0530

    Add pages

--- App.jsx
+++ App.jsx
@@ -1,12 +1,12 @@
-import { ScreenContent } from 'components/ScreenContent';
-import { StatusBar } from 'expo-status-bar';
-
+import { NavigationContainer } from '@react-navigation/native';
+import TabNavigator from 'navigator/Tabnavigator';
 import './global.css';
+
 
 export default function App() {
   return (
-    <>
- 
-    </>
+    <NavigationContainer>
+      <TabNavigator />
+    </NavigationContainer>
   );
 }
--- components/Container.jsx
+++
@@ -1,9 +0,0 @@
-import { SafeAreaView } from "react-native";
-
-export const Container = ({ children }) => {
-  return (
-    <SafeAreaView className="flex-1 m-6">
-      {children}
-    </SafeAreaView>
-  );
-};
